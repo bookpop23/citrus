8731c84989230ab9d3477484ce6378f47ee52947
bookpop23 <138991035+bookpop23@users.noreply.github.com>
2025-02-22 11:40:41 -0500

    Changed traefik labels

--- Orange/core/pgadmin/config_local.py
+++ Orange/core/pgadmin/config_local.py
@@ -1,15 +1,19 @@
+import os
+clientId = os.path.expandvars('$PGADMIN_AUTHENTIK_CLIENT_ID')
+clientSecret = os.path.expandvars('$PGADMIN_AUTHENTIK_CLIENT_SECRET')
+
 AUTHENTICATION_SOURCES = ['oauth2', 'internal']
 OAUTH2_CONFIG = [
     {
-        'OAUTH2_NAME': 'oidc',
-        'OAUTH2_DISPLAY_NAME': 'Authentik',
-        'OAUTH2_CLIENT_ID': clientID,
+        'OAUTH2_NAME': 'authentik',
+        'OAUTH2_DISPLAY_NAME': 'authentik',
+        'OAUTH2_CLIENT_ID': clientId,
         'OAUTH2_CLIENT_SECRET': clientSecret,
-        'OAUTH2_TOKEN_URL': 'https://pgadmin.citrus.reidsprite.com/application/o/token/',
-        'OAUTH2_AUTHORIZATION_URL': 'https://pgadmin.citrus.reidsprite.com/application/o/authorize/',
-        'OAUTH2_API_BASE_URL': 'https://pgadmin.citrus.reidsprite.com/',
-        'OAUTH2_USERINFO_ENDPOINT': 'https://pgadmin.citrus.reidsprite.com/application/o/userinfo/',
-        'OAUTH2_SERVER_METADATA_URL': 'https://pgadmin.citrus.reidsprite.com/application/o/pgadmin/.well-known/openid-configuration',
+        'OAUTH2_TOKEN_URL': 'https://auth.reidsprite.com/application/o/token/',
+        'OAUTH2_AUTHORIZATION_URL': 'https://auth.reidsprite.com/application/o/authorize/',
+        'OAUTH2_API_BASE_URL': 'https://auth.reidsprite.com/',
+        'OAUTH2_USERINFO_ENDPOINT': 'https://auth.reidsprite.com/application/o/userinfo/',
+        'OAUTH2_SERVER_METADATA_URL': 'https://auth.reidsprite.com/application/o/pgadmin/.well-known/openid-configuration',
         'OAUTH2_SCOPE': 'openid email profile',
         'OAUTH2_ICON': 'fa-lock',
         'OAUTH2_BUTTON_COLOR': '#000000'
